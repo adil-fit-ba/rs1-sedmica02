b9c395b2a7f1d3e80d42b36734719d34721ecea1
Adil Joldic <adil.joldic@config.ba>
2025-10-27 13:04:28 +0100

    radimo na products api

--- backend/V05_Market.Backend/Market.Application/Modules/Catalog/Products/Queries/ListProductsQueryDto.cs
+++ backend/V05_Market.Backend/Market.Application/Modules/Catalog/Products/Queries/ListProductsQueryDto.cs
@@ -4,5 +4,9 @@ public sealed class ListProductsQueryDto
 {
     public required int Id { get; init; }
     public required string Name { get; init; }
+    public required string? Description { get; set; }
+    public required decimal Price { get; set; }
+    public required int StockQuantity { get; set; }
+    public required string CategoryName { get; set; } //<-- treba poboljsati
     public required bool IsEnabled { get; init; }
 }
