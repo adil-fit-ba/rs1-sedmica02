a12c057faac1f54f61838367112cf3cc3570f9ce
Adil Joldic <adil.joldic@config.ba>
2025-11-11 16:05:46 +0100

    dorada nako nastave za poboljsanje performansi: filtrirati samo proizvode koji su u request.Items

--- V07_Market.Backend/Market.Application/Modules/Sales/Orders/Commands/Create/CreateOrderCommandHandler.cs
+++ V07_Market.Backend/Market.Application/Modules/Sales/Orders/Commands/Create/CreateOrderCommandHandler.cs
@@ -21,9 +21,15 @@ public class CreateOrderCommandHandler(IAppDbContext ctx, IAppCurrentUser curren
         #endregion
 
         #region Load products from database and prepare a map
+
+        // pokupiti sve id-ove proizvoda koji se naručuju
+        HashSet<int> productIds = request.Items.Select(ri => ri.ProductId).ToHashSet();
+
         List<ProductEntity> products = await ctx.Products
-             .AsNoTracking()
-             .ToListAsync(ct); //<--- moze poboljsati za peformanse: vratiti samo proizvode koji su u request.Items
+            .Where(p => productIds.Contains(p.Id)) //<-- dorada nakon nastave za poboljsanje performansi:
+                                                      //  filtrirati samo proizvode koji su u request.Items
+            .AsNoTracking()
+            .ToListAsync(ct); //<--- moze poboljsati za peformanse: vratiti samo proizvode koji su u request.Items
 
         Dictionary<int, ProductEntity> productsMap = products.ToDictionary(x => x.Id);
         #endregion
