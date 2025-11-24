9311bebc0a47c029bfa408258421ad3a7e329837
Adil Joldic <adil@fit.ba>
2025-11-24 14:18:45 +0100

    sedmica 09 - kraj 2. časa

---
+++ V08_Market/V08c_NativeHtmlJs/example1/example1.js
@@ -0,0 +1,47 @@
+let counter = 0;
+let running = false;
+let counterDisplay = null;
+let slider = null;
+let sliderValue = null;
+
+function onInit(){
+    counterDisplay = document.querySelector("#counterDisplay");
+    slider = document.querySelector("#speedSlider");
+    sliderValue = document.querySelector("#sliderValue");
+
+    if (counterDisplay == null)
+        throw 'counterDisplay == null';
+    if (slider == null)
+        throw 'slider == null';
+    if (sliderValue == null)
+        throw 'sliderValue == null';
+
+    slider.oninput = () => {
+        console.log(` Nova vrijednost za varijablu slider.value = ${slider.value}.`)
+        sliderValue.textContent = slider.value;
+    };
+}        
+
+function tick() {
+    if (!running) return;
+
+    counter++;
+    counterDisplay.value = counter;
+
+    // svaki put nanovo uzima interval iz slidera
+    const interval = parseInt(slider.value, 10);
+    console.log(`Novi setTimeout za interval = ${interval}.`)
+    setTimeout(tick, interval);
+}
+
+function startTimer() {
+    if (running) return;
+    running = true;
+    tick();
+}
+
+function stopTimer() {
+    running = false;
+}
+
+document.addEventListener("DOMContentLoaded", onInit);
