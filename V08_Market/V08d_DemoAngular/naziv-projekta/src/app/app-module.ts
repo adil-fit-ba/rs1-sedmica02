d29df921f2588f49451e653d5002869dab9415cb
Adil Joldic <adil@fit.ba>
2025-11-24 15:13:12 +0100

    sedmica 9 - kraj trećeg časa

--- V08_Market/V08d_DemoAngular/naziv-projekta/src/app/app-module.ts
+++ V08_Market/V08d_DemoAngular/naziv-projekta/src/app/app-module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Example1Component } from './example1/example1.component';
+import { Example2Component } from './example2/example2.component';
+import { Example3Component } from './example3/example3.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 
 @NgModule({
   declarations: [
     App,
+    Example1Component,
+    Example2Component,
+    Example3Component,
+    ErrorPageComponent,
   ],
   imports: [
     BrowserModule,
---
+++ V08_Market/V08d_DemoAngular/naziv-projekta/src/app/example1/example1.component.ts
@@ -0,0 +1,58 @@
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+  selector: 'app-example1',
+  standalone: false,
+  templateUrl: './example1.component.html',
+  styleUrl: './example1.component.scss',
+})
+export class Example1Component implements OnInit {
+
+
+  ngOnInit(): void {
+    this.counterDisplay = document.querySelector("#counterDisplay");
+    this.slider = document.querySelector("#speedSlider");
+    this.sliderValue = document.querySelector("#sliderValue");
+
+    if (this.counterDisplay == null)
+      throw 'counterDisplay == null';
+    if (this.slider == null)
+      throw 'slider == null';
+    if (this.sliderValue == null)
+      throw 'sliderValue == null';
+
+    this.slider.oninput = () => {
+      console.log(` Nova vrijednost za varijablu slider.value = ${this.slider.value}.`)
+      this.sliderValue.textContent = this.slider.value;
+    };
+  }
+
+  counter = 0;
+  running = false;
+  counterDisplay:any = null;
+  slider:any = null;
+  sliderValue:any = null;
+
+  tick() {
+    if (!this.running) return;
+
+    this.counter++;
+    this.counterDisplay!.value = this.counter;
+
+    // svaki put nanovo uzima interval iz slidera
+    const interval = parseInt(this.slider.value, 10);
+    console.log(`Novi setTimeout za interval = ${interval}.`)
+    setTimeout(this.tick, interval);
+  }
+
+  startTimer() {
+    if (this.running) return;
+    this.running = true;
+    this.tick();
+  }
+
+  stopTimer() {
+    this.running = false;
+  }
+}
