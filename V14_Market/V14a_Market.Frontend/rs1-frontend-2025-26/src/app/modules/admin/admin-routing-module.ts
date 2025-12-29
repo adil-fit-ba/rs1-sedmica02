ef27dfa9cb0b3a48f9903e8cf799eb15da4338de
Adil Joldic <adil@fit.ba>
2025-12-29 01:52:30 +0100

    FE copy paste do 13 sedmice

---
+++ V14_Market/V14a_Market.Frontend/rs1-frontend-2025-26/src/app/modules/admin/admin-routing-module.ts
@@ -0,0 +1,81 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
+import { ProductsComponent } from './catalogs/products/products.component';
+import { ProductsAddComponent } from './catalogs/products/products-add/products-add.component';
+import { ProductsEditComponent } from './catalogs/products/products-edit/products-edit.component';
+import { ProductCategoriesComponent } from './catalogs/product-categories/product-categories.component';
+import {AdminOrdersComponent} from './orders/admin-orders.component';
+import {AdminSettingsComponent} from './admin-settings/admin-settings.component';
+import {ProductCategories2Component} from './catalogs/product-categories-2/product-categories-2.component';
+import {
+  ProductCategoriesEditComponent
+} from './catalogs/product-categories-2/product-categories-edit/product-categories-edit.component';
+import {
+  ProductCategoriesAddComponent
+} from './catalogs/product-categories-2/product-categories-add/product-categories-add.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminLayoutComponent,
+    children: [
+      // PRODUCTS
+      {
+        path: 'products',
+        component: ProductsComponent,
+      },
+      {
+        path: 'products/add',
+        component: ProductsAddComponent,
+      },
+      {
+        path: 'products/:id/edit',
+        component: ProductsEditComponent,
+      },
+
+      // PRODUCT CATEGORIES
+      {
+        path: 'product-categories',
+        component: ProductCategoriesComponent,
+      },
+
+      {
+        path: 'product-categories-2',
+        component: ProductCategories2Component,
+      },
+      {
+        path: 'product-categories-2/add',
+        component: ProductCategoriesAddComponent,
+      },
+      {
+        path: 'product-categories-2/edit/:abc',
+        component: ProductCategoriesEditComponent,
+      },
+      {
+        path: 'orders',
+        component: AdminOrdersComponent,
+      },
+
+      {
+        path: 'settings',
+        component: AdminSettingsComponent,
+      },
+
+
+      // default admin route → /admin/products
+      {
+        path: '',
+        redirectTo: 'products',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AdminRoutingModule {}
